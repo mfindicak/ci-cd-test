188ea8d34b5f35ab321c1f129b741493884a8069
mertfindicak <mfindicak@gmail.com>
2023-06-24 11:35:04 +0300

    Change nestjs

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ci-cd-test';
+  title = 'Mesaj Yükleniyor...';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000').subscribe((res: any) => {
+      this.title = res.message;
+    });
+  }
 }
